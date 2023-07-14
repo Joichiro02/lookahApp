7c27492e35eec2f2736c131f20b49495dd6c17c8
Mark <markanthonyfamarin@gmail.com>
2023-07-14 17:30:22 +0800

    initialize the auth in firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,29 +1,47 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import {
-    API_KEY,
-    AUTH_DOMAIN,
-    PROJECT_ID,
-    STORAGE_BUCKET,
-    MESSAGING_SENDER_ID,
-    APP_ID,
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
 } from "react-native-dotenv";
 
 // ** firebase config
 const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGING_SENDER_ID,
-    appId: APP_ID,
-    // databaseURL: Constants.expoConfig.extra.databaseURL,
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  // databaseURL: Constants.expoConfig.extra.databaseURL,
 };
+
+const myReactNativeLocalPersistence = getReactNativePersistence({
+  getItem(...args) {
+    // Called inline to avoid deprecation warnings on startup.
+    return AsyncStorage.getItem(...args);
+  },
+  setItem(...args) {
+    // Called inline to avoid deprecation warnings on startup.
+    return AsyncStorage.setItem(...args);
+  },
+  removeItem(...args) {
+    // Called inline to avoid deprecation warnings on startup.
+    return AsyncStorage.removeItem(...args);
+  },
+});
 
 // ** initialize firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: myReactNativeLocalPersistence,
+});
 export const database = getFirestore(app);
 export const storage = getStorage(app);
